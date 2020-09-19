3710618c73ebe7062aee98eecede8f87040cf1bd
anhhminhh <58257552+anhhminhh@users.noreply.github.com>
2020-09-19 15:57:40 +0700

    a

--- script.js
+++ script.js
@@ -25,7 +25,12 @@ $(function()
 		name: "Hoa Nở Không Màu",
 		url: "Musics/HoaNoKhongMau.mp3",
 		picture: "![bg2](https://user-images.githubusercontent.com/58257552/93663066-2c605700-fa8f-11ea-90f8-3370fd4da1aa.jpg)"
-	}];
+	}, {
+		artist: "Liz Kim Cương x Trịnh Thăng Bình",
+		name: "Em Cần 1 Người Quan Tâm",
+		url: "Musics/EmCanMotNguoiQuanTâm.mp3",
+		picture: "![bg2](https://user-images.githubusercontent.com/58257552/93663066-2c605700-fa8f-11ea-90f8-3370fd4da1aa.jpg)"
+	}]
 	
 	function shuffle(a) {
 		var j, x, i;
